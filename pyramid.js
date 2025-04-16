82ef0ed226da60291426673dee656e35b7bca070
Alex Gerber <gerber031@gmail.com>
2025-04-16 20:45:38 +0200

    test

--- pyramid.js
+++ pyramid.js
@@ -1,6 +1,7 @@
 const character = "#";
 const count = 8;
 const rows = [];
+const x = "_";
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
@@ -11,6 +12,21 @@ for (let i = 1; i <= count; i++) {
 }
 
 let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
+
+
+console.log("--------------------------");
+
+
+
+for (let i = count; i > 0; i -= 1) {
+  rows.push(padRow(i, count));
+}
 
 for (const row of rows) {
   result = result + row + "\n";
